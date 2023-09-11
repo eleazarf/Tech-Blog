12bd594a05c35365d86ecef3b45d3e2fd16e8392
EleazarF@imac <eleazar.fuentes@gmail.com>
2023-09-10 18:08:45 -0600

    added authentication utility

---
+++ utils/auth.js
@@ -0,0 +1,15 @@
+// Middleware function for authentication
+const withAuth = (req, res, next) => {
+    // Check if a user is authenticated (has a userId in the session)
+    if (!req.session.userId) {
+      // If not authenticated, redirect to the login page
+      res.redirect("/login");
+    } else {
+      // If authenticated, continue to the next middleware or route handler
+      next();
+    }
+  };
+  
+  // Export the withAuth middleware for use in route handlers
+  module.exports = withAuth;
+  
